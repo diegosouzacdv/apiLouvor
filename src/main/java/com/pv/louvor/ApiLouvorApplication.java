d8f0adce97009fad9a8bf6fe4f6481cb4c1d179a
Diego <diegoguitaibanez@gmail.com>
2019-06-19 20:51:23 -0300

    Projeto criado

---
+++ src/main/java/com/pv/louvor/ApiLouvorApplication.java
@@ -0,0 +1,13 @@
+package com.pv.louvor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLouvorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLouvorApplication.class, args);
+	}
+
+}
