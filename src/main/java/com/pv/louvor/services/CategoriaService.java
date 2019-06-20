7d9217f24cdc94b6cfa345cd49978fcd0d0650af
Diego <diegoguitaibanez@gmail.com>
2019-06-20 20:48:11 -0300

    Ajustes no endpoint, e exceções de categoria

--- src/main/java/com/pv/louvor/services/CategoriaService.java
+++ src/main/java/com/pv/louvor/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.pv.louvor.model.Categoria;
 import com.pv.louvor.repositories.CategoriaRepository;
+import com.pv.louvor.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -21,6 +22,10 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
+					", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 }
