c02969cb857e76f3b6af234236c27bcd0c748089
Diego <diegoguitaibanez@gmail.com>
2019-07-06 14:03:32 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/pv/louvor/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.pv.louvor.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.pv.louvor.model.Usuario;
+import com.pv.louvor.repositories.UsuarioRepository;
+import com.pv.louvor.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private UsuarioRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Usuario user = repo.findByEmail(email);
+		if(user == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(user.getId(), user.getEmail(), user.getSenha(), user.getPerfis());
+	}
+
+}
