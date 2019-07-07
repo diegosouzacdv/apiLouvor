04eea81b188f093fcb978c258543df90462cb94e
Diego <diegoguitaibanez@gmail.com>
2019-07-07 15:05:33 -0300

    salvando primeiro arquivo no S3

--- src/main/java/com/pv/louvor/ApiLouvorApplication.java
+++ src/main/java/com/pv/louvor/ApiLouvorApplication.java
@@ -1,14 +1,18 @@
 package com.pv.louvor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.pv.louvor.services.S3Services;
 
 @SpringBootApplication
 public class ApiLouvorApplication implements CommandLineRunner{
 	
 
-
+@Autowired
+private S3Services s3;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiLouvorApplication.class, args);
@@ -16,6 +20,6 @@ public class ApiLouvorApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3.uploadFile("C:\\teste\\diego.jpg");
 }
 }
