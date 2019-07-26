d8b68488f47647b0ffd0ba30496c08adeba95649
Diego de Souza <diegoguitaibanez@gmail.com>
2019-07-26 10:09:15 -0300

    arrumando usuarioRepository

--- src/main/java/com/pv/louvor/repositories/UsuarioRepository.java
+++ src/main/java/com/pv/louvor/repositories/UsuarioRepository.java
@@ -3,7 +3,6 @@ package com.pv.louvor.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,9 +17,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 
 	UsuarioSemFuncoesDTO save(UsuarioSemFuncoesDTO obj);
 
-	Usuario findById(Integer id);
-	
-	@Query("DELETE FROM Usuario u Join u.perfis where u = 2")
-	Usuario deletePerfil();
-	
+	Usuario findById(Integer id);	
 }
