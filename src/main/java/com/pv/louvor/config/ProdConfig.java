1179caf027066c10fe18f399262424b32a99d8c5
Diego de Souza <diegoguitaibanez@gmail.com>
2019-07-26 10:15:57 -0300

    bean javaemailsende

--- src/main/java/com/pv/louvor/config/ProdConfig.java
+++ src/main/java/com/pv/louvor/config/ProdConfig.java
@@ -3,6 +3,8 @@ package com.pv.louvor.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.pv.louvor.services.EmailService;
 import com.pv.louvor.services.SmtpEmailService;
@@ -15,6 +17,12 @@ public class ProdConfig {
 	@Bean
 	public EmailService emailService() {
 		return new SmtpEmailService();
+	}
+	
+	@Bean
+	public JavaMailSender javaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		 return mailSender;
 	}
 
 }
